b3c97508e7b3a7938163a92137745d5c7af859ac
ldavid <lucasolivdavid@gmail.com>
2015-09-05 16:41:24 -0300

    Replace tabs by spaces in setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,35 @@
 from setuptools import setup, find_packages
 
 requirements = [
-	'Flask-Script',
-	'flask-restful',
-	'py2neo',
-	'cerberus',
+    'Flask-Script',
+    'flask-restful',
+    'py2neo',
+    'cerberus',
 ]
 
 test_requirements = [
-	'grapher',
-	'requests',
-	'nose',
-	'nose-parameterized',
-	'fake-factory',
-	'coverage',
-	'radon',
-	'coveralls'
+    'grapher',
+    'requests',
+    'nose',
+    'nose-parameterized',
+    'fake-factory',
+    'coverage',
+    'radon',
+    'coveralls'
 ]
 
 setup(
     name='grapher',
     version='0.1',
     license='MIT',
-	url='https://github.com/lucasdavid/grapher',
-	author='Lucas David',
-	author_email='lucasdavid@drexel.edu',
-	description='RESTful APIs creator based on resource schematics.',
+    url='https://github.com/lucasdavid/grapher',
+    author='Lucas David',
+    author_email='lucasdavid@drexel.edu',
+    description='RESTful APIs creator based on resource schematics.',
     long_description=open('README.md').read(),
-    
-	classifiers=[
-		'Framework :: Flask-RESTful',
+
+    classifiers=[
+        'Framework :: Flask-RESTful',
         'Programming Language :: Python',
         'License :: MIT',
         'Natural language :: English',
@@ -41,7 +41,7 @@ setup(
     packages=find_packages(include=('grapher', 'grapher.*')),
     include_package_data=True,
 
-	install_requires=requirements,
-	tests_require=test_requirements,
+    install_requires=requirements,
+    tests_require=test_requirements,
     test_suite='tests'
 )
