fa2ab67a59f48831c843c8e8d714800f9e0d47bf
ldavid <lucasolivdavid@gmail.com>
2015-11-15 13:32:29 -0200

    Fix DataParser
       Incorrect call to nonexistent method get_data().

--- grapher/parsers/data.py
+++ grapher/parsers/data.py
@@ -2,19 +2,19 @@ import abc
 from flask_restful import request
 
 from .base import Parser
-from ..commons import CollectionHelper
+from .. import errors, commons
 
 
 class DataParser(Parser, metaclass=abc.ABCMeta):
     @classmethod
     def parse(cls):
-        d, _ = CollectionHelper.transform(request.get_json())
+        d, _ = commons.CollectionHelper.enumerate(request.get_json())
 
         return d
 
     @classmethod
     def parse_or_raise(cls):
-        d = cls.get_data()
+        d = cls.parse()
 
         if not d:
             raise errors.BadRequestError('DATA_CANNOT_BE_EMPTY')
